02ad3a23556bb667b9f1de584d7f38756e516ad5
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-14 21:54:33 +0900

    change .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:storybook/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'tailwindcss'],
+  plugins: ['@typescript-eslint', 'tailwindcss', 'vitest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
@@ -22,5 +22,24 @@ module.exports = {
       },
     ],
     'tailwindcss/classnames-order': 'off',
+    /* vitest */
+    'vitest/consistent-test-it': [
+      'error',
+      {
+        fn: 'test',
+      },
+    ],
+    'vitest/expect-expect': 'warn',
+    'vitest/lower-case-title': 'off',
+    'vitest/max-nested-describe': [
+      'error',
+      {
+        max: 2,
+      },
+    ],
+    'vitest/no-conditional-tests': 'error',
+    'vitest/no-focused-tests': 'warn',
+    'vitest/no-identical-title': 'error',
+    'vitest/no-skipped-tests': 'warn',
   },
 };
