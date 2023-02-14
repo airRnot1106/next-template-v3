a11891be476d4c2e22912336e12310942a740028
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-14 21:59:24 +0900

    change .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,99 @@ module.exports = {
     'plugin:storybook/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'tailwindcss', 'vitest'],
+  plugins: [
+    '@typescript-eslint',
+    'simple-import-sort',
+    'import',
+    'unused-imports',
+    'tailwindcss',
+    'vitest',
+  ],
+
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
   },
   rules: {
+    /* import */
+    'simple-import-sort/imports': 'off',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'pathGroups': [
+          {
+            pattern: '{react,react-dom/**,react-router-dom}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@/app/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/stores/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/providers/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/libs/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/utils/**',
+            group: 'parent',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        'pathGroupsExcludedImportTypes': ['builtin'],
+        'alphabetize': {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
     /* tailwindcss */
     'tailwindcss/no-custom-classname': [
       'warn',
