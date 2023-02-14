a41c2a385310e389de52eb92af3d32fcb28128e0
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-14 21:34:08 +0900

    install storybook
    command: npx sb init --builder webpack5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:tailwindcss/recommended',
+    'plugin:storybook/recommended',
     'prettier',
   ],
   plugins: ['@typescript-eslint', 'tailwindcss'],
