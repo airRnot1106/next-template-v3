309dfaf91ba4b5ad7086010472862696b79a38d5
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-14 21:44:08 +0900

    support next/image for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,15 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 
+import * as NextImage from 'next/image';
+
 import '@/styles/globals.css';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
